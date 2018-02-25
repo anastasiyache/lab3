ab589a7c9e404916d5a25739637b79b07f4a5657
anastasiyache <kittydon777@gmail.com>
2018-02-26 01:29:55 +0300

    data1

--- main.ру.py
+++ main.ру.py
@@ -1,7 +1,18 @@
 import json
 
-data = ['Test data',{'Structure':'Any'}]
-s = json.dumps(data)
-print(s)
+data = {
+"firstName": "Petr",
+"lastName": "Ivskii",
+"address": {
+"streetAddress": "Moskow st., 12, f. 5",
+"city": "St.Petersburg",
+"postalCode": 342009
+},
+"phoneNumbers": [
+"812 123-1234",
+"916 123-4567"
+]
+}
 
-print(json.loads(s))
+with open('result.json', 'w') as outfile:
+    json.dump(data, outfile)
